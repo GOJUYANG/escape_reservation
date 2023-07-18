e045c3cca2a86debab4f49fa662bbcfbc6c1e55d
GOJUYANG <gjy9622@gmail.com>
2023-07-18 21:33:24 +0900

    데이터.PY 수정필요

--- DATA/data_read.py
+++ DATA/data_read.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 from PyQt5.QtWidgets import QApplication
 
@@ -6,14 +7,43 @@ import pandas as pd
 
 class DataRead:
     def __init__(self):
+
+        # 데이터 베이스 연결
         self.conn = sql3.connect("data.db", check_same_thread=False)
+
+        self.now = datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
+
+    def commit_db(self):
+        self.conn.commit()
 
     def init_tables(self):
 
-        # 회원정보 내역 테이블
-        self.conn.executescript("""
+        # 회원정보 내역 테이블 생성 (!)자료형 입력하기(!)
+        script = f"""
         CREATE TABLE 'USER' (
-        'USER_NO')""")
+        'USER_NO' INTEGER,
+        'USER_ID' VARCHAR(20),
+        'USER_PW', 
+        'USER_NM',
+        'USER_EMAIL',
+        'JOIN_DATE',
+        'USER_IMG',
+        PRIMARY KEY('USER_NO' AUTOINCREMENT) );
+        
+        CREATE TABLE 'THEME_INFO' (
+        '
+        )
+
+        CREATE TABLE 'RESERVATION' (
+        )
+                
+        INSERT INTO USER VALUES ('admin','1234','관리자','rhrnaka@gamil.com','{self.now}','../IMG/profile/manager.png');
+       
+        """
+
+        self.conn.executescript(script)
+
+        self.commit_db()
 
     def db_create(self, user_id):
         pass
