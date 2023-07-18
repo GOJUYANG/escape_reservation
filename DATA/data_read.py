9780b98be9bc7c6a451ab0aa3807af1304ae6a02
GOJUYANG <gjy9622@gmail.com>
2023-07-18 19:47:47 +0900

    Initial commit

---
+++ DATA/data_read.py
@@ -0,0 +1,15 @@
+import sys, os
+
+from PyQt5.QtWidgets import QApplication
+
+import sqlite3
+import pandas as pd
+
+class DataRead:
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    DATA = QApplication(sys.argv)
+    DATA.exec()
