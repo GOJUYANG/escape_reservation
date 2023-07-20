120fbee81243b3b8f28e31e74dfa87752c7f86ae
GOJUYANG <gjy9622@gmail.com>
2023-07-20 21:15:55 +0900

    4일차

--- CODE/MAIN/DataClass.py
+++ CODE/MAIN/DataClass.py
@@ -1,114 +1,145 @@
-from datetime import datetime
+import pandas
 
+# --- 사용자 요청
+# 사용자의 채팅 전송 요청
+class ReqUserChat:
+    def __init__(self, user_id: str, msg_type: str, user_nm: str, msg: str, send_time: str):
+        self.user_id = user_id
+        self.msg_type = msg_type
+        self.msg = msg
+        self.send_time = send_time
 
-# 클래스 이름 시작 단어 기준으로 구분
-# Req : 클라이언트 → 서버
-# Per : 서버 → 클라이언트
+# 아이디 중복확인 검사요청
+class ReqDuplicateCheck:
+    def __init__(self, id_:str):
+        self.id_ = id_
 
-# 참여 채팅방 정보를 불러와서 대화에 참여하고 있는 개인 채팅방 목록을 불러온다.
-# 채팅방 번호(타입), 채팅방 멤버
-class CallSchatList:
-    def __init__(self, chat_room_id: str, chat_room_members: list):
-        self.chat_room = chat_room_id
-        self.chat_room_members = chat_room_members
+# 유효 이메일 확인
+class ReqValidEmail:
+    def __init__(self, email_addr: str):
+        self.email_addr = email_addr
 
-# 참여 채팅방 정보를 불러와서 대화에 참여하고 있는 단체 채팅방 목록을 불러온다.
-# 채팅방 번호(타입), 채팅방 멤버
-class CallGchatList:
-    def __init__(self, chat_room_id: str, chat_room_members: list):
-        self.chat_room = chat_room_id
-        self.chat_room_members = chat_room_members
+# 사용자 회원가입 요청
+class ReqJoin:
+    def __init__(self, id_: str, pw: str, nm: str, email: str, c_date: str, img: str):
+        self.id_ = id_
+        self.pw = pw
+        self.nm = nm
+        self.email = email
+        self.c_date = c_date
+        self.img = img
 
-# 참가자가 입장할 때 채팅방에 접속인원을 알려준다.
-# 참여멤버
-class ReqJoinMember:
-    def __init__(self, my_id: str, members: list):
-        self.my_id = my_id
-        self.members = members
+# 사용자의 구독알림 요청
+class ReqSubTheme:
+    def __init__(self, user_id:str, req_date: str, th_nm: str, th_time: str):
+        self.user_id = user_id
+        self.req_date = req_date
+        self.th_nm = th_nm
+        self.th_time = th_time
 
-# 채팅 송신 허가 응답
-# 발신 아이디, 메세지
-class PerChat:
+# 사용자의 예약확인 요청
+class ReqResUserOK:
+    def __init__(self, user_id:str, user_addr:str, req_date: str, th_nm: str, th_date: str, th_time: str, th_price: str,
+                 th_num: str, th_name: str, th_phone: str, th_paytype: str):
+        self.user_id = user_id
+        self.user_addr = user_addr
+        self.req_date = req_date
+        self.th_nm = th_nm
+        self.th_date = th_date
+        self.th_time = th_time
+        self.th_price = th_price
+        self.th_num = th_num
+        self.th_name = th_name
+        self.th_phone = th_phone
+        self.th_paytype = th_paytype
+
+# --- 관리자 요청
+# 관리자의 채팅 전송 요청
+class ReqMngChat:
+    def __init__(self, manager_id: str, msg: str):
+        self.manager_id = manager_id
+        self.msg = msg
+
+# 관리자의 채팅방 리스트 요청
+class CallAllChat:
+    def __init__(self, res_no:int, res_id, res_ip, last_date, last_nm):
+        self.res_no = res_no
+        self.res_id = res_id
+        self.res_ip = res_ip
+        self.last_date = last_date
+        self.last_nm = last_nm
+
+# 관리자의 예약 / 구독 승인 요청 + 이메일 전송
+class ResMngOk:
+    def __init__(self, r_email: str, e_type: str):
+        self.r_email = r_email
+        self.e_type = e_type
+
+# 관리자의 예약수정 요청
+class ReqResFix:
+    def __init__(self, th_nm: str, th_date: str, th_time: str, th_price: str,
+                 th_num: str, th_name: str, th_phone: str, th_paytype: str):
+        self.th_nm = th_nm
+        self.th_date = th_date
+        self.th_time = th_time
+        self.th_price = th_price
+        self.th_num = th_num
+        self.th_name = th_name
+        self.th_phone = th_phone
+        self.th_paytype = th_paytype
+
+# 관리자의 예약취소 요청
+class ReqResCancel:
+    def __init__(self, th_nm: str, th_date: str, th_time: str, th_price: str,
+                 th_num: str, th_name: str, th_phone: str, th_paytype: str):
+        self.th_nm = th_nm
+        self.th_date = th_date
+        self.th_time = th_time
+        self.th_price = th_price
+        self.th_num = th_num
+        self.th_name = th_name
+        self.th_phone = th_phone
+        self.th_paytype = th_paytype
+
+# --- 공통 요청
+
+# 로그인 요청
+class ReqLogin:
+    def __init__(self, login_id: str, login_pw: str):
+        self.login_id = login_id
+        self.login_pw = login_pw
+
+# 사용자, 관리자의 채팅 조회 요청
+class ChatCNT:
+    def __init__(self, user_id:str):
+        self.user_id = user_id
+
+# 사용자, 관리자의 로그아웃 요청
+class Logout:
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+
+# 사용자, 관리자의 채팅 삭제 요청
+class DeleteChat:
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+
+# --- 서버의 허가응답
+
+# 채팅 송수신 응답
+class Perchat:
     def __init__(self, sender_id: str, msg: str):
         self.sender_id = sender_id
         self.msg = msg
-
-# 유저가 입력한 메세지가 아이디, 번호, 내용, 시간이 db에 저장된다.
-# 아이디, 번호, 내용, 시간
-class ReqSaveChat:
-    def __init__(self, id: str, msg: str, time: str):
-        self.id = id
-        self.msg = msg
-        self.time = time
-
-# 친구요청을 하면 요청자와 친구 아이디를 넘겨준다.
-# (요청자 아이디, 수락자 아이디), 허락여부
-class PlusFriend:
-    def __init__(self, id_tuple: tuple, result: bool):
-        self.id_tuple = id_tuple
-        self.result = result
-
-# 로그아웃을 하면 접속상태를 0으로 변경시킨다.
-class ReqLogout:
-    def __init__(self, id: str):
-        self.id = id
-
-# 사용자의 프로필과 상태메세지를 변경할 수 있다.
-# 유저아이디, 상태메세지
-class ReqStateChange:
-    def __init__(self, user_id: str, user_state: str, user_img:str):
-        self.user_id = user_id
-        self.user_state = user_state
-        self.user_img = user_img
-
-# 사용자의 채팅창 배경이미지를 변경할 수 있다.
-# 유저아이디, 채팅창 배경이미지 경로
-# class ReqImgChange:
-#     def __init__(self, user_id: str, user_chatimg_path: str):
-#         self.user_id = user_id
-#         self.user_chatimg_path = user_chatimg_path
-
-# 친구 리스트가 우측에 출력된다.
-# 사용자 프로필, 친구 이름, 접속 상태
-class ShowFrdList:
-    def __init__(self, user_profile: str, friend_name: str, is_online: bool):
-        self.user_profile: user_profile
-        self.friend_name = friend_name
-        self.is_online = is_online
-
-# 나가기를 누른 CTB_CHATROOM 테이블에서 CR_ID에 맞는 행들만 삭제한다.
-# 내 아이디, 나간 채팅방 아이디
-class DeleteMyTable:
-    def __init__(self, my_id: str, cr_id: str):
-        self.my_id = my_id
-        self.cr_id = cr_id
-
-# 1. 해당 CR_ID의 TB_CONTENT, TB_READ_CNT 테이블을 삭제한다.
-# 2. TB_CHATROOM에서 CR_ID 삭제
-# 참여멤버가 0인 채팅방 아이디
-class DeleteTable:
-    def __init__(self, cr_id: str, my_id: str, my_name:str):
-        self.cr_id = cr_id
-        self.my_id = my_id
-        self.my_name = my_name
-
-# ==================== 로그인
-
-# 로그인 요청 : 아이디, 비밀번호
-class ReqLogin:
-    def __init__(self, id_:str, pw:str):
-        self.id_ = id_
-        self.password = pw
 
 # 로그인 허가 응답 : 허가번호, 아이디
 # rescode = 0 ("아이디 존재하지 않음")
 # rescode = 1 ("비밀번호 존재하지 않음")
 # rescode = 2 ("로그인 허가 완료")
 class PerLogin:
-    def __init__(self, rescode: int, user_id_:str, login_info=[], user_info=[]):
+    def __init__(self, rescode: int, login_id: str, user_info: list):
         self.rescode = rescode
-        self.user_id_ = user_id_
-        self.login_info = login_info
+        self.login_id = login_id
         self.user_info = user_info
 
 # 로그인 유저 정보 발송
@@ -119,111 +150,42 @@ class LoginInfo:
         self.id_ = id_
         self.login = login
 
-# ==================== 로그아웃
-
-# 로그아웃 요청 : 아이디, 시간
-class ReqLoout:
-    def __init__(self, id_: str):
-        self.id_ = id_
-
-
-# ==================== 회원가입
-
-# 회원가입 중복 아이디 체크 요청 : 아이디
-class ReqDuplicateCheck:
-    def __init__(self, id_:str):
-        self.id_ = id_
-
 # 아이디 중복 체크 응답 : True(중복), False(중복아님)
 class PerDuplicateCheck:
     def __init__(self, isExisited: bool):
         self.isExisited = isExisited
 
-# 회원가입 요청 : 아이디, 비밀번호, 닉네임, 이메일, 가입일자, 프로필 이미지경로, 상태메세지
-class ReqMembership:
-    def __init__(self, id_: str, pw: str, nm: str, email: str, c_date: str, img: str):
-        self.id_ = id_
-        self.pw = pw
-        self.nm = nm
-        self.email = email
-        self.c_date = c_date
-        self.img = img
+#유효이메일 응답
+class PerValidEmail:
+    def __init__(self, isValid: bool):
+        self.isValid = isValid
 
 # 회원가입 허가 응답
-class PerRegist:
+class PerJoin:
     def __init__(self, Success: bool):
         self.Success = Success
 
-# 이메일 유효성 확인 및 전송 요청
-class ReqEmailSend:
-    def __init__(self, r_email: str):
-        self.r_email = r_email
+# 예약 승인요청 허가응답(데이터 저장)
+class PerResOk:
+    def __init__(self, savedone: bool):
+        self.savedone = savedone
 
-# 이메일 전송 응답
+# 예약수정 허가 응답
+class PerResfix:
+    def __init__(self, isfix: bool):
+        self.isfix = isfix
+
+# 예약취소 허가 응답
+class PerResCancel:
+    def __init__(self, iscancel: bool):
+        self.iscancel = iscancel
+
+# 사용자 구독요청 허가응답(데이터저장)
+class PerSubTheme:
+    def __init__(self, isSaved: bool):
+        self.isSaved = isSaved
+
+# 이메일 전송 허가 응답
 class PerEmailSend:
-    def __init__(self, isSend: bool):
-        self.isSend = isSend
-
-# 이메일 인증번호 확인 요청
-class ReqEmailNumber:
-    def __init__(self, num1: str, num2: str):
-        self.num1 = num1
-        self.num2 = num2
-
-# 이메일 인증번호 일치 여부 응답
-class PerEmailNumber:
-    def __init__(self, ismatch: bool):
-        self.ismatch = ismatch
-
-# ==================== 친구 초대
-
-# 친구 초대 요청
-class ReqSuggetsFriend:
-    def __init__(self, user_id_: str, frd_id_: str, result=0):
-        self.user_id_ = user_id_
-        self.frd_id_ = frd_id_
-        self.result = result
-
-# 친구 수락 허가 응답
-class PerAcceptFriend:
-    def __init__(self, user_id_: str, frd_id_: str, result=0):
-        self.user_id_ = user_id_
-        self.frd_id_ = frd_id_
-        self.result = result
-
-# ==================== 채팅방 개설
-
-# 채팅방 요청
-class JoinChat:
-    def __init__(self, user_id_: str, member_id: list, member_name: list, title:str, cr_id_=""):
-        self.user_id_ = user_id_
-        self.member_id = member_id
-        self.member_name = member_name
-        self.title = title
-        self.cr_id_ = cr_id_
-
-# 채팅방 나가기
-class OutChat:
-    def __init__(self, cr_id_: str, user_id_: str):
-        self.cr_id_ = cr_id_
-        self.user_id_ = user_id_
-
-def get_data_tuple(t_class):
-    data_dict = t_class.__dict__
-    data_tuple = tuple(data_dict.values())
-    return data_tuple
-
-# ==================== 메시지 송신
-
-# 채팅 송수신
-class ReqChat:
-    def __init__(self, cr_id_:str, user_id_:str, msg: str, user_nm=""): #msg = self.edt_txt.text()
-        self.cr_id_ = cr_id_
-        self.user_nm = user_nm
-        self.user_id_ = user_id_
-        self.msg = msg
-
-class ReadChat:
-    def __init__(self, cr_id_:str):
-        self.cr_id_ = cr_id_
-        self.msg:pandas.DataFrame
+    def __init__(self,send_type:str):
+        self.send_type = send_type
--- CODE/SERVER/DataRead.py
+++ CODE/SERVER/DataRead.py
@@ -1,20 +1,23 @@
 import datetime
 import sys
 from PyQt5.QtWidgets import QApplication
+from CODE.MAIN.DataClass import *
 
 import sqlite3 as sql3
 import pandas as pd
+import re
 
-class DataRea:
+class DataRead:
     def __init__(self):
 
         # --- 데이터베이스 연결
-        self.conn = sql3.connect("data.db", check_same_thread=False)
+        self.conn = sql3.connect("../SERVER/data.db", check_same_thread=False)
 
         # --- 변수 초기화 및 설정
         self.now = datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S")
 
         # --- 함수호출
+        # self.return_df('TH_NM', 'THEME_INFO')
 
     def end_conn(self):  # db 종료
         self.conn.close()
@@ -76,8 +79,8 @@ class DataRea:
         self.commit_db()
         self.end_conn()
 
-    def return_df(self, c_type: str, tb_name: str):
-        """return : 테이블"""
+    def return_df(self, c_type:str, tb_name:str):
+        """return : THEME_INFO 테이블"""
         sql = f"select {c_type} from {tb_name}"
         df = pd.read_sql(sql, self.conn)
         return df
@@ -117,5 +120,131 @@ class DataRea:
                 self.conn.execute(sql, data[k])
         self.commit_db()
 
-# if __name__ == '__main__':
-#     DATA = DataRead()
+    def membership_id_check(self, data: ReqDuplicateCheck) -> PerDuplicateCheck:
+        """클라이언트 중복 아이디 확인 요청 -> 서버 db에서 아이디 중복 여부 응답"""
+        result: PerDuplicateCheck = PerDuplicateCheck(isExisited=True)
+        sql = f"SELECT * FROM USER WHERE USER_ID = '{data.id_}'"
+        row = pd.read_sql(sql, self.conn)
+
+        if len(row) != 0:
+            result.isExisited = True
+        else:
+            result.isExisited = False
+        return result
+
+    def eamil_check(self, data: ReqValidEmail) -> PerValidEmail:
+        """클라이언트 이메일 주소 유효 확인 요청"""
+        result: PerValidEmail = PerValidEmail(isValid=True)
+        if re.match(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', data.r_email):
+            result = PerValidEmail(True)
+        else:
+            result = PerValidEmail(False)
+        return result
+
+    def membership_regist(self, data: ReqJoin) -> PerJoin:
+        result: PerJoin = PerJoin(True)
+        try:
+            sql = f"INSERT INTO USER (USER_ID, USER_PW, USER_NM, USER_EMAIL, JOIN_DATE, USER_IMG)" \
+                  f"VALUES ('{data.id_}','{data.pw}','{data.nm}','{data.email}','{data.c_date}','{data.img}')"
+            self.conn.execute(sql)
+            self.conn.commit()
+
+        except:
+            self.conn.rollback()
+            result.Success = False
+        finally:
+            self.conn.close()
+        return result
+
+    def login(self, data: ReqLogin) -> PerLogin:
+        print("[ login ]")
+        """클라이언트 로그인 요청 -> 서버 로그인 허가 """
+        result: PerLogin = PerLogin(rescode=2, login_id=data.login_id)
+        sql = f"SELECT * FROM USER WHERE USER_ID = '{data.login_id}' AND USER_PW = '{data.login_pw}'"
+        print(sql)
+        df = pd.read_sql(sql, self.conn)
+        row = len(df)
+        print("row",row)
+
+        if row in [None, 0]:
+            result.rescode = 0
+        # 입력한 아이디와 비밀번호, db에서 가진 아이디와 비밀번호
+        # elif data.id_ != row[1] or data.password != row[2]:
+        #     result.rescode = 1
+        else:
+            result.rescode = 2
+        return result
+
+    def insert_message(self, data):
+        print(f"[ 메세지 저장..]")
+        self.conn.execute(f"insert into CHATROOM_{data.user_id} (SEND_ID, MSG_TYPE, MESSAGE, LAST_SEND_TIME)"
+                          f" values (?,?,?,?)",
+                          (data.user_id, data.msg_type, data.msg, data.send_time))
+        self.commit_db()
+        print(f" [메세지 저장 완료] ")
+
+    def save_subtb(self, data: ReqSubTheme) -> PerSubTheme:
+        print("[ 구독 요청 저장 완료 ]")
+        result: PerSubTheme = PerSubTheme(isSaved=True)
+        try:
+            self.conn.execute(f"insert into SUBSCRIBE (USER_ID, Req_Date, TH_NM, TH_TIME) "
+                              f"values (?,?,?,?)",
+                              (data.user_id, data.req_date, data.th_nm, data.th_time))
+            self.commit_db()
+            print("[ 구독 요청 저장 완료 ]")
+        except:
+            self.conn.rollback()
+            result.Success = False
+        finally:
+            self.conn.close()
+        return result
+
+    def send_email(self, data: ResMngOk) -> PerEmailSend:
+        result: PerEmailSend = PerEmailSend('')
+        if data.e_type == '예약':
+            result = PerEmailSend('reservation')
+        elif data.e_type == '구독':
+            result = PerEmailSend('subscribe')
+        return result
+
+    def save_restb(self, data: ReqResUserOK) -> PerResOk:
+        print("[ 예약 내역 저장.. ]")
+        result: PerResOk = PerResOk(savedone=True)
+        try:
+            self.conn.execute(f"insert into RESERVATION (USER_ID, USER_ADDR, Req_Date,"
+                              f"RES_TH, RES_DATE, RES_TIME, RES_PRICE, RES_NUM, RES_REP_NM, RES_PHONE, RES_PAYTYPE) "
+                              f"values (?,?,?,?,?,?,?,?,?,?,?)",
+                              (data.user_id, data.user_addr, data.req_date,
+                               data.th_nm, data.th_date, data.th_time, data.th_price,
+                               data.th_num, data.th_phone, data.th_paytype))
+            self.commit_db()
+            print("[ 예약 내역 저장 완료 ]")
+        except:
+            self.conn.rollback()
+            result.savedone = False
+        finally:
+            self.conn.close()
+        return result
+
+    def get_chatlist_by_id(self):
+        pass
+        #tablewidget에 뿌려질 개설된 채팅방 리스트를 그대로 가져와야함
+        #RESERVATION, CHATROOM_{ID} JOIN이 필요함
+        #
+
+    # 예약테이블 행 수정
+    def update_restb(self, data):
+        pass
+
+    # 예약테이블 행 삭제
+    def delete_restb(self, data):
+        pass
+
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    DATA = DataRead()
+    App.exec()
+
+    # 유저만 필요한 부분, 관리자는 필요없다.
+    # self.insert_message(ReqUserChat(f"'{data.nm}'님이 입장했습니다."))
